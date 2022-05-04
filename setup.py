d870df7d8f01bf2110bd5f778ea34435426d2a57
sejin-chun <101098015+sejin-chun@users.noreply.github.com>
2022-05-04 09:26:54 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name='mycalculator0001',
+  version='1.0.0',
+  description='oss-dev my calculator',
+  author='sjchun',
+  author_email='sjchun@donga.ac.kr',
+  url = 'https://github.com/sejin-chun/mycalculator/',
+  install_requires=['pytest'],
+  long_description= 'oss-dev my calc',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+      "Programming Language :: Python :: 3",
+      "Operating System :: OS Independent",
+  ],
+)
